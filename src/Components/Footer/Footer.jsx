57f0b2d3e12ef6ff12db8261b0392a7cbc8acb6a
kingsteven1113 <125849798+kingsteven1113@users.noreply.github.com>
2025-01-29 17:30:04 +0000

    ...

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Footer.css'
+
+
+const Footer = () => {
+
+    return (
+        <>
+        <div className="Footer">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30085.168071164004!2d-73.66692208166586!3d41.19425916477821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2a5367a60362b%3A0x541edf9049a80fb1!2sBedford%2C%20NY%2010506!5e1!3m2!1sen!2sus!4v1738170666085!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        </>
+    )
+
+}
+
+export default Footer;
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './LandingPage.css'
+
+
+const LandingPage = () => {
+
+    return (
+        <>
+        <div className="HeroSection">
+        Hello!
+        </div>
+        </>
+    )
+
+}
+
+export default LandingPage;
